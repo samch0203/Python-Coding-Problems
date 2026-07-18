ed3ce6ee434d7e82ac112990642ed0d29154abc4
SAMIKSHA CHIBADE <samiksha.chibade23@mmit.edu.in>
2026-07-18 21:09:56 +0530

    Add exception handling for division and input errors

---
+++ exception.py
@@ -0,0 +1,10 @@
+t = int(input())
+
+for _ in range(t):
+    try:
+        a, b = map(int, input().split())
+        print(a // b)
+    except ZeroDivisionError:
+        print("Error Code: integer division or modulo by zero")
+    except ValueError as e:
+        print("Error Code:", e)
